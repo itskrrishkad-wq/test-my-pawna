0ce997bea303955b44327abd6a62def2a433417e
krishkad <krishkadam88@gmail.com>
2026-07-19 15:20:37 +0530

    fix the next.config.ts to accept unsplash.com

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // images: {
+  //   remotePatterns: ["images.unsplash.com"],
+  // },
+
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
 export default nextConfig;
-
